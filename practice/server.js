6e4a2733e768306e3ad395aff01bae15d54611e7
Yogesh55S <Yogesharma914@gmail.com>
2025-01-29 22:15:26 +0530

    work

--- practice/server.js
+++ practice/server.js
@@ -1,20 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const rootDir = __dirname;
 
 const userRouter = require("./user");
 const hostRouter = require("./host");
 
-
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(userRouter);
 app.use(hostRouter);
+app.use(express.static(path.join(rootDir, 'public')));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.status(404).send(`
     <h1> 404 Page not found
     `);
 });
-PORT = 3001;
-app.listen(PORT, ()=>{
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- practice/user.js
+++ practice/user.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const userRouter = express.Router();
 userRouter.get("/",(req,res,next)=>{
-  res.sendFile(path.join(__dirname,"./","file.html"));
+  res.sendFile(path.join(__dirname,"./","views","index.html"));
 })
 
 module.exports = userRouter;
