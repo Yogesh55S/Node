6e423f5c2d7523dd690ba60d78d4489a521864e4
Yogesh55S <Yogesharma914@gmail.com>
2025-01-29 13:09:53 +0530

    work

--- practice/host.js
+++ practice/host.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const hostRouter = express.Router();
 
-hostRouter.get("/host/add-home",(req,res,next)=>{
+hostRouter.get("/add-home",(req,res,next)=>{
   res.send(`
     <h1>Welcome to house Registration</h1>
-    <form action="/host/add-home" method="post">
+    <form action="/add-home" method="post">
     <input type="text" name="name" placeholder="Enter your name"> <br>
     <input type="text" name="house-name" placeholder="Enter your house-name"> <br>
     <input type="submit" value="Submit">
   </form>
     `);
 })
-hostRouter.post("/host/add-home",(req,res,next)=>{
+hostRouter.post("/add-home",(req,res,next)=>{
   console.log(req.body);
   res.send(`
     <h1>Home registrer succesfully</h1>
--- practice/user.js
+++ practice/user.js
@@ -1,10 +1,8 @@
+const path = require("path");
 const express = require("express");
 const userRouter = express.Router();
 userRouter.get("/",(req,res,next)=>{
-  res.send(`
-    <h1>hello friday</h1>
-    <a href="/host/add-home" >add home</a>
-    `);
+  res.sendFile(path.join(__dirname,"./","file.html"));
 })
 
 module.exports = userRouter;
