be0302fba52d4a758f3f4cc53edac1de3717b18f
Yogesh55S <Yogesharma914@gmail.com>
2025-03-16 23:41:15 +0530

    work

--- assign/controller/hostcontroller.js
+++ assign/controller/hostcontroller.js
@@ -1,51 +1,49 @@
 const Home = require("../models/file");
 
 //--That is for Home page rendering
-exports.gethome = (req, res, next) => {
+exports.gethome=(req,res,next)=>{
   console.log("Now you are on Add Page");
-  res.render('Host/Edit-Home', {
-    editing: false,
-  });
-};
-
-exports.getedithome = (req, res, next) => {
-  const homeId = req.params.homeId;
-  const editing = req.query.editing === 'true';
-  Home.findById(homeId, house => {
-    if (!house) {
-      console.log("home not found");
-      return res.redirect("Host/Homedit");
-    }
-    console.log(homeId, editing, house);
-    res.render('Host/Edit-Home', {
-      house: house,
-      editing: editing,
+    res.render('Host/Edit-Home',{
+      editing:false,
     });
-  });
-};
-
+  }
+ 
+  exports.getedithome=(req,res,next)=>{
+    const homeId = req.params.homeId;
+    const editing = req.query.editing === 'true';
+    Home.findById(homeId,house=>{
+      if(!house){
+        console.log("home not found");
+       return  res.redirect("Host/Homedit");
+      }
+      console.log(homeId,editing,house);
+      res.render('Host/Edit-Home', {
+        house:house,
+        editing:editing,});
+    })
+    }
 //---Add-page
-exports.addhome = (req, res, next) => {
-  const { housename, price, location, rating, imageUrl } = req.body;
-  const home = new Home(housename, price, location, rating, imageUrl);
+exports.addhome = (req,res,next)=>{
+  const {housename,price,location,rating,imageUrl} = req.body;
+  const home = new Home (housename,price,location,rating,imageUrl);
   home.save();
 
-  Home.fetchAll((registerhomes) => {
-    res.render('Host/Homedit', { registerhomes: registerhomes });
+  const registerhomes = Home.fetchAll((registerhomes)=>{
+     res.render('/Host/Homedit',{registerhomes:registerhomes});
     console.log(registerhomes);
   });
-};
+  }
+  
+  exports.Postedithome = (req,res,next)=>{
+    const {id,housename,price,location,rating,imageUrl} = req.body;
+    const home = new Home (housename,price,location,rating,imageUrl);
+    home.id = id;
+    home.save();
+    res.redirect("Host/Homedit");
+    }
 
-exports.Postedithome = (req, res, next) => {
-  const { id, housename, price, location, rating, imageUrl } = req.body;
-  const home = new Home(housename, price, location, rating, imageUrl);
-  home.id = id;
-  home.save();
-  res.redirect("Host/Homedit");
-};
-
-exports.list = (req, res, next) => {
-  Home.fetchAll((registerhomes) => {
-    res.render('Host/Homedit', { registerhomes: registerhomes });
-  });
-};
+exports.list=(req,res,next)=>{
+  const registerhomes = Home.fetchAll((registerhomes)=>{
+    res.render('Host/Homedit',{registerhomes:registerhomes});
+ }); 
+}
