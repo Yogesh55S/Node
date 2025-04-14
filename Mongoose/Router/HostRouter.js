a373d6e5ff26511244c8323d04fccd064bf8d6f4
Yogesh55S <Yogesharma914@gmail.com>
2025-04-14 23:16:35 +0530

    work

--- Mongoose/Router/HostRouter.js
+++ Mongoose/Router/HostRouter.js
@@ -5,7 +5,7 @@ const files = require("../controller/hostcontroller");
 
 hostRouter.get("/add-page",files.gethome);
 hostRouter.post("/add-page",files.addhome);
-hostRouter.get("/home-edit",files.list);
+hostRouter.get("/home-edit",files.list); 
 hostRouter.get("/edit-home/:homeId",files.getedithome);
 hostRouter.post("/edit-home",files.Postedithome);
 hostRouter.post("/delete-home/:homeId",files.deletehome);
